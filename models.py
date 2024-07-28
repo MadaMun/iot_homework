883161d8f368749890fb4ca1a5b1b9a6bd45aa18
Pongsakorn Nopparatttayaporn <pongsakorn1820@gmail.com>
2024-07-29 00:49:43 +0700

    api_finish
    all file of database

---
+++ models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+# from sqlalchemy.orm import relationship
+
+from database import Base
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    short = Column(String, index=True)
+    catagory = Column(String, index=True)
+    author = Column(String, index=True)
+    year = Column(Integer, index=True)
+    is_published = Column(Boolean, index=True)
+
+class Drink(Base):
+    __tablename__ = 'drinks'
+
+    id = Column(Integer, primary_key=True, index=True)
+    menu = Column(String, index=True)
+    prics = Column(Integer, index=True)
+
+    
+class Order(Base):
+    __tablename__ = 'orders'
+
+    drink_id = Column(Integer, ForeignKey('drinks.id'))
+    id = Column(Integer, primary_key=True, index=True)
+    menu = Column(String, index=True)
+    much = Column(Integer, index=True)
+    note = Column(String, index=True)
+
